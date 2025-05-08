872a341f13f921b036fec1cce581e1b509cb90da
m.d <49080343+000000d@users.noreply.github.com>
2025-05-07 20:45:01 -0400

    Cleanup

--- export.go
+++ export.go
@@ -1,162 +1,146 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"time"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "os"
+    "strings"
+    "time"
 )
 
 type MessageBlock struct {
-	ID        string `json:"id,omitempty"`
-	Content   string `json:"content,omitempty"`
-	ChannelID string `json:"channel_id,omitempty"`
-	Author    struct {
-		ID       string `json:"id"`
-		Username string `json:"username"`
-	} `json:"author"`
+    ID        string `json:"id,omitempty"`
+    Content   string `json:"content,omitempty"`
+    ChannelID string `json:"channel_id,omitempty"`
+    Author    struct {
+        ID       string `json:"id"`
+        Username string `json:"username"`
+    } `json:"author"`
 }
 
 type ExportedContent struct {
-	ChannelID string `json:"channel_id"`
-	Messages  []struct {
-		Content string `json:"message"`
-		UserID  string `json:"user_id"`
-		User    string `json:"user"`
-	} `json:"messages"`
+    ChannelID string `json:"channel_id"`
+    Messages  []struct {
+        Content string `json:"message"`
+        UserID  string `json:"user_id"`
+        User    string `json:"user"`
+    } `json:"messages"`
 }
 
-const g_AUTH_FILE_NAME string = "auth.txt"
-const g_API_VERSION string = "v10"
-const g_API_LIMIT uint8 = 100
-const g_SLEEP_TIME time.Duration = 500
+const AUTH_FILE_NAME string = "auth.txt"
 
-var g_channelID string = "0"
+const API_VERSION string = "v10"
+const API_LIMIT uint8 = 100
+const SLEEP_TIME time.Duration = 500 // Stated in milliseconds
 
-func LogSetup() {
-	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
-	if _, err := os.Stat("./logs"); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir("./logs", 0755)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	logFile, err := os.Create(fmt.Sprintf("./logs/%s.log", time.Now().Format("2006-01-02_15.04.05")))
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.SetOutput(logFile)
+var channelID string = "0"
+
+func exportDirSetup(channelID string) *os.File {
+    exportDirectory := "message-exports"
+    if _, err := os.Stat(exportDirectory); errors.Is(err, os.ErrNotExist) {
+        err := os.Mkdir(exportDirectory, 0755)
+        if err != nil {
+            log.Fatalf("Error creating directory '%s' to store exported messages. ERROR: %v", exportDirectory, err)
+        }
+    }
+    exportFileName := fmt.Sprintf("%s_%v.json", channelID, time.Now().Format("2006-01-02_15.04.05"))
+    exportPath := fmt.Sprintf("./%s/%s", exportDirectory, exportFileName)
+    exportFile, err := os.OpenFile(exportPath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("Error creating export file '%s'. ERROR: %v\n", exportFile,  err)
+    }
+    defer exportFile.Close()
+
+    return exportFile
 }
 
-func ExportDirSetup(g_channelID string) *os.File {
-	if _, err := os.Stat("./message-exports"); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir("./message-exports", 0755)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	var exportFileName string = fmt.Sprintf("%s_%v.json", g_channelID, time.Now().Format("2006-01-02_15.04.05"))
-	var exportPath string = fmt.Sprintf("./message-exports/%s", exportFileName)
-	exportFile, err := os.OpenFile(exportPath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer exportFile.Close()
+func apiCall(baseAPI, auth string, exportFile *os.File, exported ExportedContent) {
+    prevMessageID := "0"
+    client := &http.Client{
+        Timeout: 5 * time.Second,
+    }
+    var msgJSONList []MessageBlock
 
-	return exportFile
-}
+    req, err := http.NewRequest("GET", baseAPI, nil)
+    if err != nil {
+        log.Fatalln("Error creating new HTTP request. ERROR:", err)
+    }
+    req.Header.Set("Authorization", auth)
 
-func Call(APIUrl, auth string, exportFile *os.File, exported ExportedContent) {
-	var prevMessageID string = "0"
-	var client *http.Client = &http.Client{
-		Timeout: 5 * time.Second,
-	}
-	var msgJSONList []MessageBlock
+    res, err := client.Do(req)
+    if err != nil {
+        log.Fatalln("Error sending HTTP request. ERROR:", err)
+    }
+    defer res.Body.Close()
 
-	req, err := http.NewRequest("GET", APIUrl, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Authorization", auth)
+    resBody, err := io.ReadAll(res.Body)
+    if err != nil {
+        log.Fatalln("Error reading HTTP response body. ERROR:", err)
+    }
 
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
+    err = json.Unmarshal(resBody, &msgJSONList)
+    if err != nil {
+        log.Fatalf("Incorrect auth token. Received: '%s'\n", auth)
+    }
 
-	resBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+    for _, message := range msgJSONList {
+        prevMessageID = message.ID
+        exported.Messages = append(exported.Messages, struct {
+            Content string `json:"message"`
+            UserID  string `json:"user_id"`
+            User    string `json:"user"`
+        }{
+            Content: message.Content,
+            UserID:  message.Author.ID,
+            User:    message.Author.Username,
+        })
+    }
 
-	err = json.Unmarshal(resBody, &msgJSONList)
-	if err != nil {
-		log.Printf("incorrect auth / token. received: '%s'", auth)
-		log.Fatal(err)
-	}
+    if len(msgJSONList) < int(API_LIMIT) {
+        export, err := json.Marshal(exported)
+        if err != nil {
+            log.Fatalln("Error with JSON marshal. ERROR:", err)
+        }
+        err = os.WriteFile(exportFile.Name(), export, os.ModePerm)
+        if err != nil {
+            log.Fatalf("Error writing exported messages to file '%s'. ERROR: %v\n", exportFile.Name(),  err)
+        }
+        log.Println("Finished")
+        os.Exit(0)
+    }
 
-	for _, message := range msgJSONList {
-		prevMessageID = message.ID
-		exported.Messages = append(exported.Messages, struct {
-			Content string `json:"message"`
-			UserID  string `json:"user_id"`
-			User    string `json:"user"`
-		}{
-			Content: message.Content,
-			UserID:  message.Author.ID,
-			User:    message.Author.Username,
-		})
-	}
+    beforeParamAPI := fmt.Sprintf("https://discord.com/api/%s/channels/%s/messages?limit=%d&before=%s", API_VERSION, channelID, API_LIMIT, prevMessageID)
 
-	if len(msgJSONList) < int(g_API_LIMIT) {
-		export, err := json.Marshal(exported)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = os.WriteFile(exportFile.Name(), export, os.ModePerm)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("finished")
-		os.Exit(0)
-	}
-
-	var beforeParamAPIUrl string = fmt.Sprintf("https://discord.com/api/%s/channels/%s/messages?limit=%d&before=%s", g_API_VERSION, g_channelID, g_API_LIMIT, prevMessageID)
-
-	time.Sleep(g_SLEEP_TIME * time.Millisecond)
-	Call(beforeParamAPIUrl, auth, exportFile, exported)
+    time.Sleep(SLEEP_TIME * time.Millisecond)
+    apiCall(beforeParamAPI, auth, exportFile, exported)
 }
 
 func main() {
-	LogSetup()
+    if len(os.Args) < 2 {
+        fmt.Fprintf(os.Stderr, "Usage: %s <channel_id> ...\n", os.Args[0])
+        fmt.Fprintf(os.Stderr, "ERROR: Include channel ID as CLI arg\n")
+        os.Exit(1);
+    }
 
-	if len(os.Args) < 2 {
-		log.Fatal("pass channel id as cli arg.")
-	} else if len(os.Args) > 2 {
-		log.Fatal("Current limited to exporting one channel at a time.")
-	}
+    readAuthFile, err := os.ReadFile(AUTH_FILE_NAME)
+    if err != nil || len(readAuthFile) == 0 {
+        os.Create(AUTH_FILE_NAME)
+        fmt.Fprintf(os.Stderr, "File '%s' must contain Discord token to export messages\n", AUTH_FILE_NAME)
+        os.Exit(1)
+    }
 
-	readAuthFile, err := os.ReadFile(g_AUTH_FILE_NAME)
-	if err != nil || string(readAuthFile) == "" {
-		os.Create(g_AUTH_FILE_NAME)
-		log.Printf("paste discord token inside '%s'.", g_AUTH_FILE_NAME)
-		os.Exit(-1)
-	}
+    auth := strings.Trim(string(readAuthFile), "\n")
+    channelID = os.Args[1]
+    baseAPI := fmt.Sprintf("https://discord.com/api/%s/channels/%s/messages?limit=%d", API_VERSION, channelID, API_LIMIT)
 
-	// var auth string = strings.Trim(string(readAuthFile), "\n")
-	var auth string = string(readAuthFile)
-	g_channelID = os.Args[1]
-	var baseAPIUrl string = fmt.Sprintf("https://discord.com/api/%s/channels/%s/messages?limit=%d", g_API_VERSION, g_channelID, g_API_LIMIT)
+    var exported ExportedContent
+    exported.ChannelID = channelID
+    exportFile := exportDirSetup(channelID)
 
-	var exported ExportedContent
-	exported.ChannelID = g_channelID
-	var exportFile *os.File = ExportDirSetup(g_channelID)
-
-	log.Printf("started on channel: '%s', api version: '%s', limit: '%d'", g_channelID, g_API_VERSION, g_API_LIMIT)
-	Call(baseAPIUrl, auth, exportFile, exported)
+    log.Printf("Started on channel: '%s', API version: '%s', limit: '%d'", channelID, API_VERSION, API_LIMIT)
+    apiCall(baseAPI, auth, exportFile, exported)
 }
